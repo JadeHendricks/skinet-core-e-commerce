2e4bfa26bbc33155370657d46795ef619e95b8dc
Jade <hendricksjade@gmail.com>
2023-05-10 01:11:47 +0200

    add(endpoint): adding in code to get the product brands and types back.

--- Core/Interfaces/IProductRepository.cs
+++ Core/Interfaces/IProductRepository.cs
@@ -7,5 +7,7 @@ namespace Core.Interfaces
         Task<Product> GetProductByIdAsync(int id);
         //IReadOnlyList - we can be spcific here as we don't need the extra methods that come with list, we only want to read and display them
         Task<IReadOnlyList<Product>> GetProductsAsync();
+        Task<IReadOnlyList<ProductBrand>> GetProductBrandsAsync();
+        Task<IReadOnlyList<ProductType>> GetProductTypesAsync();
     }
 }
