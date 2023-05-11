cd32f77648c891c98812215010b113d87e980cba
Jade <hendricksjade@gmail.com>
2023-05-11 18:45:05 +0200

    refactor(startup): storing the services to ApplicationServicesExtension and calling one line inside of the program.cs startup file.

--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,6 @@
-using API.Errors;
+using API.Extensions;
 using API.Middleware;
-using Core.Interfaces;
 using Infrastructure.Data;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,35 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-//implementing StoreContext as a service
-builder.Services.AddDbContext<StoreContext>(options => {
-    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-
-builder.Services.AddScoped<IProductRepository, ProductRepository>();
-//we don't have a type yet for this specific server, it gets passed to it when needed
-builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
-builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-
-//We overriding the behaviour of the [ApiController] here to shape the model state into something we want to send back
-builder.Services.Configure<ApiBehaviorOptions>(options => {
-    options.InvalidModelStateResponseFactory = actionContext => 
-    {
-        var errors = actionContext.ModelState
-            .Where(e => e.Value.Errors.Count > 0)
-            .SelectMany(x => x.Value.Errors)
-            .Select(x => x.ErrorMessage).ToArray();
-
-        var errorResponse = new ApiValidationErrorResponse {
-            Errors = errors
-        };
-
-        return new BadRequestObjectResult(errorResponse);
-    };
-});
+builder.Services.AddApplicationServices(builder.Configuration);
 
 var app = builder.Build();
 
