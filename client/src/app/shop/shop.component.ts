30a8efab20c89d18c8565e9b7598b37a93181c75
Jade <hendricksjade@gmail.com>
2023-05-21 18:53:11 +0200

    add(filter): adding in the filter functionality to the shopping component

--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Product } from '../shared/models/product';
 import { ShopService } from './shop.service';
+import { Type } from '../shared/models/type';
+import { Brand } from '../shared/models/brand';
 
 @Component({
   selector: 'app-shop',
@@ -8,13 +10,47 @@ import { ShopService } from './shop.service';
   styleUrls: ['./shop.component.scss']
 })
 export class ShopComponent {
-
-  products: Product[] = [];
+  public products: Product[] = [];
+  public brands: Brand[] = [];
+  public types: Type[] = [];
+  
+  public brandIdSelected = 0;
+  public typeIdSelected = 0;
 
   constructor(private shopService: ShopService) {
-    this.shopService.getProducts().subscribe({
+    this.getProducts();
+    this.getBrands();
+    this.getTypes();
+   }
+
+   private getProducts(): void {
+    this.shopService.getProducts(this.brandIdSelected, this.typeIdSelected).subscribe({
       next: response => this.products = response.data,
       error: error => console.log(error)
     });
+   }
+
+   private getBrands(): void {
+    this.shopService.getBrands().subscribe({
+      next: response => this.brands = [{ id: 0, name: 'All' }, ...response],
+      error: error => console.log(error)
+    });
+   }
+
+   private getTypes(): void {
+    this.shopService.getTypes().subscribe({
+      next: response => this.types = [{ id: 0, name: 'All' }, ...response],
+      error: error => console.log(error)
+    });
+   }
+
+   public onBrandSelected(brandId: number): void {
+    this.brandIdSelected = brandId;
+    this.getProducts();
+   }
+
+   public onTypeSelected(typeId: number): void {
+    this.typeIdSelected = typeId;
+    this.getProducts();
    }
 }
